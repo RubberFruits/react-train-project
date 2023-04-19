7d5882f073270f65cd1d0f90e7fdd505a3ba1beb
Vladimir <zagarskih@internetlab.ru>
2023-04-19 21:43:44 +0400

    jest, first test [2]

---
+++ config/jest/jest.config.ts
@@ -0,0 +1,24 @@
+const config = {
+    clearMocks: true,
+    testEnvironment: 'jsdom',
+    coveragePathIgnorePatterns: [
+        '\\\\node_modules\\\\',
+    ],
+    moduleDirectories: [
+        'node_modules',
+    ],
+    moduleFileExtensions: [
+        'js',
+        'jsx',
+        'ts',
+        'tsx',
+        'json',
+        'node',
+    ],
+    testMatch: [
+        '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
+    ],
+    rootDir: '../../',
+};
+
+export default config;
